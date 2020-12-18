badf71ea5a4f21802b31926ef364009677614cd6
yeomsteve <jjojjosteve@gmail.com>
2020-12-18 11:24:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zeksbe-jauqah
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zeksbe-jauqah)
---
+++ src/app/indesign/indesign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indesign',
+  templateUrl: './indesign.component.html',
+  styleUrls: ['./indesign.component.css']
+})
+export class IndesignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
